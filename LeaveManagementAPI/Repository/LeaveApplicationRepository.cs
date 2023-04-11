7c8c4e722e21b527b44d7e85a0f0ea2764cd78c6
mwmullins2 <mwmullins2@icloud.com>
2023-04-11 18:21:52 -0400

    Added to leave application interface and repository.

--- LeaveManagementAPI/Repository/LeaveApplicationRepository.cs
+++ LeaveManagementAPI/Repository/LeaveApplicationRepository.cs
@@ -1,8 +1,55 @@
-﻿using LeaveManagementAPI.Interfaces;
+﻿using LeaveManagementAPI.Data;
+using LeaveManagementAPI.Interfaces;
+using LeaveManagementTool.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace LeaveManagementAPI.Repository
 {
     public class LeaveApplicationRepository: ILeaveApplicationRepository
     {
+        private readonly DataContext _context;
+
+        public LeaveApplicationRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public ICollection<LeaveApplication> GetLeaveApplications()
+        {
+            return _context.LeaveApplications.ToList();
+        }
+
+        public LeaveApplication GetLeaveApplicationById(int leaveId)
+        {
+            return _context.LeaveApplications.Where(l => l.Id == leaveId).FirstOrDefault();
+        }
+
+        public bool LeaveApplicationExists(int leaveId)
+        {
+            return _context.LeaveApplications.Any(l => l.Id == leaveId);
+        }
+
+        public bool CreateLeaveApplication(LeaveApplication leaveApplication)
+        {
+            _context.Add(leaveApplication);
+            return Save();
+        }
+
+        public bool UpdateLeaveApplication(LeaveApplication leaveApplication)
+        {
+            _context.Update(leaveApplication);
+            return Save();
+        }
+
+        public bool DeleteLeaveApplication(LeaveApplication leaveApplication)
+        {
+            _context.Remove(leaveApplication);
+            return Save();
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
     }
 }
